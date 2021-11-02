7a9d95a11a88a776460381ebedd907206c3bb55c
muktar ahmed <muktar.ahmed13@yahoo.com>
2021-11-02 16:17:28 -0500

    added question in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 const startButton =document.getElementById("start-btn")
 const questionContainerEl = document.getElementById("question-container")
+const questionEl = document.getElementById("question")
+const answerButtonEl = document.getElementById("answer-button")
 // const startButton =document.getElementById("start-btn")
 
 startButton.addEventListener("click", startGame)
@@ -10,12 +12,24 @@ nextQuestion()
 }
 
 function nextQuestion(){
-
+showQuestion()
+}
+function showQuestion(question){
+questionEl.innerText = question.question
+console.log("question should show up")
 }
 
 function selectAnswer(){
 
 }
-
+var questions = [
+    {
+        question: "guess the answer what?",
+        answer:[
+            {text: "what", correct: true},
+            {text: "yes", correct: false}
+        ]
+    }
+]
 
 
