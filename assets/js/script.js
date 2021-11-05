1f7de5fe7266512ee861ce8c14e5a022e177bdae
muktar ahmed <muktar.ahmed13@yahoo.com>
2021-11-05 16:48:51 -0500

    added loop for the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,11 @@
+var currentQuestionIndex = 0;
+
 var startButton = document.getElementById("start-btn")
 var quesionContainerEl = document.getElementById("question-container")
 var nextButton = document.getElementById("next-btn")
 var intro = document.getElementById("intro")
 var quesionEl = document.getElementById("question")
+var answerEl = document.getElementById("answer-button")
 
 startButton.addEventListener("click", startGame)
 function startGame (){
@@ -16,14 +19,26 @@ nextQuestion()
 nextButton.addEventListener("click", nextQuestion)
 function nextQuestion(){
     nextButton.classList.remove("hide")
-displayQuestion()
+    
+    if (
+      currentQuestionIndex < questions.length ){
+        displayQuestion()
+console.log("show next questiom")
+      }
+      // will come back to this to fix
+      else {
+        console.log("game over")
+      }
+currentQuestionIndex++;
+
 }
 
 function displayQuestion(){
-    for (var i=0; i < questions.length; i++ ){
-        quesionEl.innerText = questions[i].title
+        quesionEl.innerText = questions[currentQuestionIndex].title
+        
+        // this will set answer buttons to empty
+        // answerEl.innerText="";
 
-    }
 
 }
 
@@ -31,7 +46,6 @@ function displayAnswers(){
     
 }
 
-console.log("show answer")
 
 var questions = [
     {
@@ -43,22 +57,22 @@ var questions = [
       {
         title: "Placeholder question two:",
         choices: ["answer1", "answer2", "answer3", "answer4"],
-        answer: "answer1"
+        answer: "answer2"
       },
       {
         title: "Placeholder question three:",
         choices: ["answer1", "answer2", "answer3", "answer4"],
-        answer: "answer1"
+        answer: "answer3"
       },
       {
         title: "Placeholder question four:",
         choices: ["answer1", "answer2", "answer3", "answer4"],
-        answer: "answer1"
+        answer: "answer4"
       },
       {
         title: "Placeholder question five:",
         choices: ["answer1", "answer2", "answer3", "answer4"],
-        answer: "answer1"
+        answer: "answer3"
       },
   
     
