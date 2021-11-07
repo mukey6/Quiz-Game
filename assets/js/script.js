ef6d6cfabd6ff6707795534d7c1c6c7c4ba216ec
muktar ahmed <muktar.ahmed13@yahoo.com>
2021-11-07 13:12:07 -0600

    display highscore

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var currentQuestionIndex = 0;
-var countDown = 100;
+var countDown = 60;
 // var currentScore=0
-var clock;
+var timer;
 
 var startButton = document.getElementById("start-btn")
 var quesionContainerEl = document.getElementById("question-container")
@@ -9,26 +9,28 @@ var nextButton = document.getElementById("next-btn")
 var intro = document.getElementById("intro")
 var quesionEl = document.getElementById("question")
 var answerEl = document.getElementById("answer-button")
-var timer = document.getElementById("timer")
+var time = document.getElementById("timer")
 var gameOver = document.getElementById("end-game")
-// var score = document.getElementById("score")
+var submitBtn = document.getElementById("submit")
+var initialEl = document.getElementById("initials")
+var feedbackEl = document.getElementById("feedback");
+
 
 startButton.addEventListener("click", startGame)
 function startGame (){
 startButton.classList.add("hide")
 quesionContainerEl.classList.remove("hide")
 intro.classList.add("hide")
-timer.textContent=countDown;
+time.textContent=countDown;
 // score.innerText = currentScore
 
 
 
-//need to stop timer 
-clock = setInterval(function(){
+//need to stop time 
+timer = setInterval(function(){
   if ( countDown > 0){
     countDown--;
-    timer.textContent=countDown;
-    console.log("you have " + countDown + "secs" )
+    time.textContent=countDown;
   } else {
     endGame()
   } 
@@ -58,37 +60,38 @@ console.log("show next questiom")
 }
 function displayQuestion() {
   document.getElementById("answer-button").innerHTML=""
-  // var id = 1
+ 
   quesionEl.innerText = questions[currentQuestionIndex].title
   questions[currentQuestionIndex].choices.forEach(choice => {
-    // console.log(choice);
     let button = document.createElement("button")
             button.innerText=choice
             answerEl.appendChild(button)
           button.className = "btn myClass"
-          // button.onclick=selectedAnswer(document.querySelector(`.btn${id}`))
-          // console.log(document.querySelector(`.btn${id}`).value)
 
-// id++
   })
 }
 answerEl.addEventListener("click", selectedAnswer)
 
 function selectedAnswer(event){
-  // score.innerText = currentScore
-var answerSelected = event.target.innerText
+
+  var answerSelected = event.target.innerText
 if (answerSelected != questions[currentQuestionIndex].answer){
 
 countDown-=5
-  console.log("wrong answer")
-// currentScore --
-//   console.log(currentScore + "you losts")
+  feedbackEl.textContent = "WRONG!";
+
 } else{
 
-  console.log("right answer")
-  // currentScore ++ 
-  // console.log("you gained" + currentScore)
+  feedbackEl.textContent = "CORRECT!";
+
 }
+
+// right/wrong feedback on page 
+feedbackEl.setAttribute("class", "feedback");
+setTimeout(function() {
+  feedbackEl.setAttribute("class", "feedback hide");
+}, 500);
+
 nextQuestion()
 }
 function endGame(){
@@ -100,48 +103,73 @@ function endGame(){
 
   gameOver.classList.remove("hide")
 
-  clearInterval(clock)
-  console.log("should stop time")
+  clearInterval(timer)
+  console.log("should stop time", countDown)
   finalScoreEl.textContent = countDown;
+  time.classList.add("hide")
 
   
 }
+
+var saveHighScore = function (){
+  initials = initialEl.value
+
+  if (initials !== "") {
+
+  var highScore =
+  JSON.parse(window.localStorage.getItem("highScore")) || [];
+
+
+var newScore = {
+  score: countDown,
+  initials: initials
+}
+
+highScore.push(newScore);
+window.localStorage.setItem("highScore", JSON.stringify(highScore))
+
+window.location.href = "highscore.html";
+}
+}
+
+
+// click on submit button 
+submitBtn.onclick= saveHighScore
+console.log(submitBtn + "you clicked submit")
+
+//list of questions
 var questions = [
-    {
-        title: "Which Vikings player had the most sacks in 2018 season?",
-        choices: ["Everson Griffen ", "Brian Robison ", "Danielle Hunter", "Harrison Smith"],
-        answer: "Danielle Hunter"
-      },
-
-      {
-        title: "Which quarterback had nine starts for the Vikings in 2013?",
-        choices: ["Christain Ponder", "Case Keenum", "Kirk Cousins", "Geno Smith"],
-        answer: "Christain Ponder"
-      },
-      {
-        title: "Which team did the Vikings play in London during the 2013 season?",
-        choices: ["Raiders", "Rams", "Steelers", "Texans"],
-        answer: "Steelers"
-      },
-      {
-        title: "What was the last team the Vikings played at the Metrodome?",
-        choices: ["Ravens", "Lions", "Bears", "Packers"],
-        answer: "Lions"
-      },
-      {
-        title: "Which Vikings WR set the single-season rookie touchdown reception record?",
-        choices: ["Randy Moss", "Chris Carter", "Adam Theilen", "Kyle Rudolph"],
-        answer: "Randy Moss"
-      },
-  
-    {
-      title:"How Many Times Have the Vikings Gone to the Super Bowl?",
-      choices: ["8", "4", "2", "0"],
-      answer: "4"
+  {
+      title: "Which Vikings player had the most sacks in 2018 season?",
+      choices: ["Everson Griffen ", "Brian Robison ", "Danielle Hunter", "Harrison Smith"],
+      answer: "Danielle Hunter"
     },
-    
-]
-// function FinalScore(){
 
-// }
-// localStorage.setItem.json
+    {
+      title: "Which quarterback had nine starts for the Vikings in 2013?",
+      choices: ["Christain Ponder", "Case Keenum", "Kirk Cousins", "Geno Smith"],
+      answer: "Christain Ponder"
+    },
+    {
+      title: "Which team did the Vikings play in London during the 2013 season?",
+      choices: ["Raiders", "Rams", "Steelers", "Texans"],
+      answer: "Steelers"
+    },
+    {
+      title: "What was the last team the Vikings played at the Metrodome?",
+      choices: ["Ravens", "Lions", "Bears", "Packers"],
+      answer: "Lions"
+    },
+    {
+      title: "Which Vikings WR set the single-season rookie touchdown reception record?",
+      choices: ["Randy Moss", "Chris Carter", "Adam Theilen", "Kyle Rudolph"],
+      answer: "Randy Moss"
+    },
+
+  {
+    title:"How Many Times Have the Vikings Gone to the Super Bowl?",
+    choices: ["8", "4", "2", "0"],
+    answer: "4"
+  },
+  
+]
